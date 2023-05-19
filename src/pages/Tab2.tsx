5af7cb76e843d3bfaf8e403c57eded5ed63b0e98
GiovanniAdb <78819879+GiovanniAdb@users.noreply.github.com>
2023-05-18 18:53:10 -0600

    Se agrego el boton para camera

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,5 +1,20 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
+import { camera, trash, close } from 'ionicons/icons';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonFab,
+  IonFabButton,
+  IonIcon,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonImg,
+  IonActionSheet,
+} from '@ionic/react';
 import './Tab2.css';
 
 const Tab2: React.FC = () => {
@@ -7,10 +22,15 @@ const Tab2: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
+          <IonTitle>Photo Gallery</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
+      <IonFab vertical="bottom" horizontal="center" slot="fixed">
+    <IonFabButton onClick={() => takePhoto()}>
+      <IonIcon icon={camera}></IonIcon>
+    </IonFabButton>
+  </IonFab>
         <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">Tab 2</IonTitle>
